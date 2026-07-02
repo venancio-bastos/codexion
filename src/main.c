15b5bee66822166445601def624e1e753fd39b53
venancio-bastos <vjpb2002@gmail.com>
2026-07-02 23:51:09 +0100

    Basic setup

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char *mandatory_args[] = {
+    "number_of_coders",
+    "time_to_burnout",
+    "time_to_compile",
+    "time_to_debug",
+    "time_to_refactor",
+    "number_of_compiles_required",
+    "dongle_cooldown",
+    "scheduler"
+};
+
+int main(int ac, char **av) {
+
+    if (ac == 0)
+    {
+        printf("Must pass the following list of args to the function: [FAZER PRINT DOS VALORES]");
+    }
+    // Verificar se todos os argumentos são passados
+    
+    //Executar o projeto
+    
+
+    return 0;
+}
